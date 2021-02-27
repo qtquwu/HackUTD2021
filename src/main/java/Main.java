95a2c4fca0b97587da34b7e627cf140e3c513bbe
Quinn <quinn.t.quinley@gmail.com>
2021-02-27 13:55:58 -0600

    Updated Main, build.gradle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,8 @@
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class Main {
+    public static void main(String[] args) {
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+    }
 }
