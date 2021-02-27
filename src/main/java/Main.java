3836560206f03234707860ad9d5a838d2aece1dd
Quinn <quinn.t.quinley@gmail.com>
2021-02-27 14:57:29 -0600

    Ping Pong

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,28 +9,16 @@ import javax.security.auth.login.LoginException;
 
 public class Main extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
-        JDABuilder builder = new JDABuilder(AccountType.BOT);
-        String token = "(INSERT BOT TOKEN HERE!)";
-        builder.setToken(token);
+        String token = "ODE1MzEzNjI0NjgzOTA1MDI0.YDql-w.-on7XNlIwA6slobA1QvdDQUffi4";
+        JDABuilder builder = JDABuilder.createDefault(token);
         builder.addEventListeners(new Main());
         builder.build();
     }
 
     @Override
-    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+    public void onMessageReceived(MessageReceivedEvent event) {
         System.out.println("Received message!");
         if(event.getMessage().getContentRaw().equals("!ping")) {
-            event.getChannel().sendMessage("Pong!").queue();
-        }
-    }
-
-
-    // Not working with Guilds yet - figure out how
-    @Override
-    public void onGenericGuildMessage(@NotNull GenericGuildMessageEvent event) {
-        super.onGenericGuildMessage(event);
-        System.out.println(event.getMessageId());
-        if(event.getMessageId().equals("!ping")) {
             event.getChannel().sendMessage("Pong!").queue();
         }
     }
